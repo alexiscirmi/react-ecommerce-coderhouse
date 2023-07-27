1056812ce01e7da7b8affea5577db0fbebaaa8f8
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-07-26 21:10:21 -0300

    Working on navbar

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,30 +1,29 @@
 function Navbar() {
   return (
     <div>
-      <header className="navbar navbar-expand-lg d-flex flex-wrap align-items-center justify-content-center justify-content-md-between mb-4 border-bottom">
+      <header className="navbar navbar-expand-lg fixed-top mb-4 border-bottom">
 
-        <nav className="container-fluid container-lg">
+        <nav className="container d-flex align-items-center justify-content-between">
 
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
 
-          <ul className="collapse navbar-collapse navbar-nav col-4 mb-2 justify-content-center mb-md-0" id="navbarToggler">
+          <ul className="collapse navbar-collapse navbar-nav col-lg-4 mb-2 justify-content-center mb-md-0 order-2 order-lg-0" id="navbarToggler">
             <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Inicio</a></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Galería</a></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Precios</a></li>
             <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Contacto</a></li>
           </ul>
 
-          <div className="col-4 mb-2 mb-md-0 text-center">
+          <div className="col-lg-4 mb-2 mb-md-0 text-center">
             <a href="#" className="navbar-brand d-inline-flex link-body-emphasis text-decoration-none py-0 mx-0">
               Marcotel
             </a>
           </div>
 
-          <div className="col-4 text-end">
-            <button type="button" className="btn btn-outline-secondary me-2">Entrar</button>
-            <button type="button" className="btn btn-outline-secondary">Registrarse</button>
+          <div className="col-lg-4 text-center">
+            <button type="button" className="btn btn-outline-secondary">Ingresar</button>
           </div>
 
         </nav>
