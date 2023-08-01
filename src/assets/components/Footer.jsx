b2bdf155656be58f7bd74db623f9db8480e76f16
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-08-01 01:08:40 -0300

    UI tweaks

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,32 +1,31 @@
 function Footer() {
   return (
-    <footer className="footer container d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
+    <footer className="footer container d-flex flex-wrap justify-content-between align-items-center py-3 border-top mb-3">
 
-      <div className="col-md-4 d-flex align-items-center">
-        <a href="#" className="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
-
-        </a>
-
-        <p className="mb-3 mb-md-0 text-body-secondary">
+      <div className="col-md-4 d-flex">
+        <p className="text-body-secondary mb-0">
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-c-circle" viewBox="0 0 16 16">
             <path d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8Zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0ZM8.146 4.992c-1.212 0-1.927.92-1.927 2.502v1.06c0 1.571.703 2.462 1.927 2.462.979 0 1.641-.586 1.729-1.418h1.295v.093c-.1 1.448-1.354 2.467-3.03 2.467-2.091 0-3.269-1.336-3.269-3.603V7.482c0-2.261 1.201-3.638 3.27-3.638 1.681 0 2.935 1.054 3.029 2.572v.088H9.875c-.088-.879-.768-1.512-1.729-1.512Z" />
           </svg>
           <a href="https://github.com/cirmialexis/coderhouse-react-project" target="_blank" className="nav-link-footer ps-1 text-body-secondary">Alexis Cirmi</a>
         </p>
-
       </div>
+
+      <a href="#" className="col-md-4 d-flex justify-content-center text-body-secondary text-decoration-none allison fs-1">
+        M
+      </a>
 
       <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
         <li className="ms-3"><a className="text-body-secondary" href="#">
-          <i className="bi bi-facebook" />
+          <i className="bi bi-facebook fs-4" />
         </a></li>
 
         <li className="ms-3"><a className="text-body-secondary" href="#">
-          <i className="bi bi-instagram" />
+          <i className="bi bi-instagram fs-4" />
         </a></li>
 
         <li className="ms-3"><a className="text-body-secondary" href="#">
-          <i className="bi bi-whatsapp" />
+          <i className="bi bi-whatsapp fs-4" />
         </a></li>
       </ul>
 
--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar() {
   return (
-    <header className="navbar navbar-expand-lg fixed-top mb-4 border-bottom">
+    <header className="navbar navbar-expand-md fixed-top mb-4 border-bottom">
 
       <nav className="container-fluid d-flex align-items-center justify-content-around">
 
@@ -8,20 +8,19 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        <ul className="collapse navbar-collapse navbar-nav col-lg-4 order-2 order-lg-0 justify-content-center gap-4 gap-lg-0 my-3 my-lg-0" id="navbarToggler">
+        <ul className="collapse navbar-collapse navbar-nav col-md-4 order-2 order-md-0 justify-content-center gap-4 gap-md-0 my-3 my-md-0" id="navbarToggler">
           <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Inicio</a></li>
           <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Galería</a></li>
           <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Precios</a></li>
-          <li className="nav-item"><a href="#" className="nav-link px-2 link-secondary">Contacto</a></li>
         </ul>
 
-        <div className="col-lg-4 text-center">
+        <div className="col-md-4 text-center">
           <a href="#" className="navbar-brand d-inline-flex link-body-emphasis text-decoration-none py-0 mx-0">
             Marcotel
           </a>
         </div>
 
-        <div className="col-lg-4 text-center">
+        <div className="col-md-4 text-center">
           <button type="button" className="btn btn-outline-secondary">Ingresar</button>
         </div>
 
