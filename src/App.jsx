1f1b4545ba0fd34fb64e43bf8796e7ba6ab93138
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-08-07 12:06:51 -0300

    Footer component deleted

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import './assets/scss/_colors.scss'
 import './App.scss'
 import Navbar from './components/Navbar/Navbar.jsx';
 import ItemListContainer from './containers/ItemListContainer/ItemListContainer';
-import Footer from './components/Footer/Footer.jsx';
 
 function App() {
   return (
     <div className='App'>
       <Navbar />
       <ItemListContainer greeting='Hola mundo' />
-      <Footer />
     </div>
   );
 }
