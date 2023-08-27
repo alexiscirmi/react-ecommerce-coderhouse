c4ba7e6842e8c79ee2104cb83f0cec51ca77c471
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-08-27 17:52:32 -0300

    Cards are working

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -4,7 +4,7 @@ import { CartFill } from 'react-bootstrap-icons';
 function CartWidget() {
   return (
     <div className='cart-widget-container'>
-      <div className='cart-widget-amount'>7</div>
+      <div className='cart-widget-amount'>0</div>
       <div className='cart-icon-mobile'>
         <CartFill className='fs-4' />
       </div>
--- src/containers/ItemListContainer/ItemListContainer.jsx
+++ src/containers/ItemListContainer/ItemListContainer.jsx
@@ -4,39 +4,47 @@ import './_item-list-container.scss';
 
 function ItemListContainer({ greeting }) {
 
-  // const [list, setList] = useState([]);
-  // const params = useParams();
+  const [list, setList] = useState([]);
+  const params = useParams();
 
-  // const getItems = async () => {
-  //   const response = await fetch('https://fakestoreapi.com/products');
-  //   const items = await response.json();
-  //   const filteredItems = items.filter((item) => item.category === params.id);
-  //   setList(items);
-  // };
+  const getItems = async () => {
+    const response = await fetch('https://fakestoreapi.com/products');
+    const items = await response.json();
+    const filteredItems = items.filter((item) => item.category === params.id);
+    setList(items);
+  };
 
-  // useEffect(getItems(), [params]);
+  useEffect(() => {
+    getItems()
+  }, [params]);
 
   return (
     <div className='item-list-container container-fluid container-lg'>
-      <div className='d-flex flex-row flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3'>
+      <div className='d-flex flex-row flex-wrap row-cols-1 row-cols-md-2 row-cols-lg-3 justify-content-center'>
 
-        <div className='d-flex col justify-content-center my-4'>
-          <div className='card'>
-            <div className='mt-1'>
-              <img src='https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg' className='card-img-top' alt='...' />
-            </div>
-            <div className='card-body'>
-              <h5 className='card-title'>Card title</h5>
-              <p className='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-              <div className='d-flex justify-content-between'>
-                <a href='#' className='btn btn-primary'>Ver descripción</a>
-                <button className='btn btn-primary'>-</button>
-                <span className='btn'>0</span>
-                <button className='btn btn-primary'>+</button>
+        {
+          list.map((item) => (
+            <div key={item.id} className='d-flex col justify-content-center my-4'>
+              <div className='card'>
+                <div className='mt-1 image-container'>
+                  <img src={item.image} className='card-img-top' alt='...' />
+                </div>
+                <div className='card-body d-flex flex-column justify-content-between'>
+                  <h5 className='card-title'>{item.title}</h5>
+                  <div className='border-top'>
+                    <p className='card-text fs-4 mt-1 mb-2'>$ {item.price}</p>
+                    <div className='d-flex justify-content-between'>
+                      <div className='btn btn-primary'>Ver descripción</div>
+                      <button className='btn btn-primary'>-</button>
+                      <span className='btn'>0</span>
+                      <button className='btn btn-primary'>+</button>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          ))
+        }
 
       </div>
     </div >
