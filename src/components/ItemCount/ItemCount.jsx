da5ddbfee0e18a356161ff636fb3c12b0ac0388d
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-08-28 20:20:37 -0300

    ItemCount is working

--- src/components/ItemCount/ItemCount.jsx
+++ src/components/ItemCount/ItemCount.jsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
+
 function ItemCount() {
+
+  const [amount, setAmount] = useState(0);
+
+  const subtractAmount = () => {
+    amount > 0 && setAmount(amount - 1);
+  }
+
+  const addAmount = () => {
+    setAmount(amount + 1);
+  }
+
   return (
     <div>
-      <button className='btn btn-primary'>-</button>
-      <span className='btn'>0</span>
-      <button className='btn btn-primary'>+</button>
+      <button className='btn btn-primary' onClick={subtractAmount}>-</button>
+      <span className='btn'>{amount}</span>
+      <button className='btn btn-primary' onClick={addAmount}>+</button>
     </div>
   );
 }
