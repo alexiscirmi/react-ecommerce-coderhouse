d9e7ebb137f7003b86264ff13f84ec702ca8e725
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-08-28 22:53:03 -0300

    UI tweaks

--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -5,13 +5,13 @@ function ItemDetail({ detail }) {
 
   return (
     <main className='item-detail container-fluid container-lg'>
-      <div className='d-flex flex-row mt-4'>
+      <div className='d-block row d-md-flex flex-md-row mt-4'>
 
-        <div className='col-md-6'>
+        <div className='text-center col-md-6'>
           <img src={detail.image} alt={detail.title} className='border rounded item-image' />
         </div>
 
-        <div className='text-end balance col-md-6'>
+        <div className='text-center text-md-end balance col-md-6 mt-4 mt-md-0 mb-5'>
           <h1 className='fs-2'>{detail.title}</h1>
           <h2 className='fs-5 item-category'>{detail.category}</h2>
           <p>{detail.description}</p>
