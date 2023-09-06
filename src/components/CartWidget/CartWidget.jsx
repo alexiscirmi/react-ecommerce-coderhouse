43c8b381fbf40b0596b72daa96159b89fe0b8803
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-09-06 01:39:18 -0300

    Working on SCSS modules

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,18 +1,18 @@
 import { useContext } from 'react'
 import { CartFill } from 'react-bootstrap-icons'
 import { CartContext } from '../context/cartContext'
-import './_cart-widget.scss'
+import styles from './CartWidget.module.scss'
 
 function CartWidget() {
 
   const { cartCount } = useContext(CartContext)
 
   return (
-    <div className='cart-widget-container'>
-      <div className='cart-widget-amount'>
+    <div className={styles.cartWidgetContainer}>
+      <div className={styles.cartWidgetAmount}>
         {cartCount}
       </div>
-      <div className='cart-icon-mobile'>
+      <div>
         <CartFill className='fs-4' />
       </div>
     </div >
