0767747a9fec578b24ffbb405419c4f11dd8f4c8
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-09-17 12:57:34 -0300

    Minor adjustments to fit project's requirements

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,29 +5,48 @@ import styles from './Cart.module.scss'
 
 function Cart() {
 
-  const { cart } = useContext(CartContext)
+  const { cart, clear, addItem, removeItem } = useContext(CartContext)
 
-  return (
-    <div className={`pb-5 ${styles.cart}`}>
-      {
-        cart.map((item) => {
-          return (
-            <div key={item.id} className={styles.cartItem}>
-              <div className={styles.cartItemImage}>
-                <img src={item.image} className={styles.img} />
+  if (cart.length > 0) {
+    return (
+      <div className={`pb-5 ${styles.cart}`}>
+        {
+          cart.map((item) => {
+            return (
+              <div key={item.id} className={styles.cartItem}>
+                <div className={styles.cartItemImage}>
+                  <img src={item.image} className={styles.img} />
+                </div>
+                <span>{item.title}</span>
+                <span className='d-none d-lg-block'>Precio: $ {item.price.toLocaleString("es-AR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                <span>Total: $ {(item.quantity * item.price).toLocaleString("es-AR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
+                <div>
+                  <button className={`btn btn-primary`} onClick={removeItem}>-</button>
+                  <span>{item.quantity}</span>
+                  <button className={`btn btn-primary`} onClick={addItem}>+</button>
+                </div>
+
               </div>
-              <span>{item.title}</span>
-              <span>Cantidad: {item.quantity}</span>
-              <span>Precio: $ {item.price.toLocaleString("es-AR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-              <span>Total: $ {(item.quantity * item.price).toLocaleString("es-AR", { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</span>
-            </div>
-          )
-        })
-      }
+            )
+          })
+        }
 
-      <Link to='/checkout' className='mt-3 mx-auto btn btn-primary'>Continuar</Link>
-    </div>
-  );
+        <div className='d-flex gap-3 justify-content-center mt-4'>
+          <button className='btn btn-primary' onClick={clear}>Limpiar carrito</button>
+          <Link to='/checkout' className='btn btn-primary'>Continuar</Link>
+        </div>
+      </div>
+    )
+  } else {
+    return (
+      <div className='d-flex flex-column align-items-center justify-content-center text-center h-100'>
+        <h1 className='fs-4'>No hay ítems en el carrito</h1>
+        <Link to='/' className='btn btn-primary mt-5'>Volver al inicio</Link>
+      </div>
+    )
+  }
+
+
 }
 
 export default Cart;
