139600edd49e7965863b0802acf41359b2179f7f
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-10-16 18:15:47 -0300

    Minor UI fixes

--- src/components/CheckoutForm/CheckoutForm.jsx
+++ src/components/CheckoutForm/CheckoutForm.jsx
@@ -3,22 +3,22 @@ import styles from './CheckoutForm.module.scss'
 function CheckoutForm({ handleName, handlePhone, handleEmail, sendOrder }) {
 
   return (
-    <div className={styles.checkout}>
+    <div className={`pb-5 ${styles.checkout}`}>
       <h1 className='fs-4 mt-3'>Finalizar compra</h1>
       <p className='mt-4 mx-4 text-center balance'>Complete el formulario con sus datos. Todos los campos son requeridos.</p>
       <form action='' className={`mt-1 ${styles.form}`}>
 
-        <fieldset className='d-flex flex-column justify-content-center gap-2 my-4'>
+        <fieldset className='d-flex flex-column justify-content-center gap-2 my-3'>
           <label className='form-label' htmlFor='nombre'>Nombre</label>
           <input className='form-control' type='text' id='nombre' placeholder='Linus Torvalds' required onChange={handleName} />
         </fieldset>
 
-        <fieldset className='d-flex flex-column justify-content-center gap-2 my-4'>
+        <fieldset className='d-flex flex-column justify-content-center gap-2 my-3'>
           <label className='form-label' htmlFor='telefono'>Teléfono (10 dígitos)</label>
           <input className='form-control' type='tel' id='telefono' minLength='10' maxLength='10' placeholder='1123456789' required onChange={handlePhone} />
         </fieldset>
 
-        <fieldset className='d-flex flex-column justify-content-center gap-2 my-4'>
+        <fieldset className='d-flex flex-column justify-content-center gap-2 my-3'>
           <label className='form-label' htmlFor='email'>Email</label>
           <input className='form-control' type='email' id='email' placeholder='linustorvalds@outlook.com' required onChange={handleEmail} />
         </fieldset>
