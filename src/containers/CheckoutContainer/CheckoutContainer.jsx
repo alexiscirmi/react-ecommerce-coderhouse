f5bc4ad9c933ad25ce37f6b39e9acf7ef9297151
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-10-18 14:00:58 -0300

    EmailJS added

--- src/containers/CheckoutContainer/CheckoutContainer.jsx
+++ src/containers/CheckoutContainer/CheckoutContainer.jsx
@@ -1,8 +1,8 @@
 import { useContext, useState } from 'react'
 import { CartContext } from '../../context/cartContext'
 import CheckoutForm from '../../components/CheckoutForm/CheckoutForm'
-import { addDoc, collection, getFirestore, doc, updateDoc } from 'firebase/firestore'
 import Order from '../../components/Order/Order'
+import { addDoc, collection, getFirestore, doc, updateDoc } from 'firebase/firestore'
 
 function Checkout() {
   const { cart, clear } = useContext(CartContext)
@@ -29,45 +29,44 @@ function Checkout() {
 
   const [orderId, setOrderId] = useState(undefined)
 
-  const sendOrder = async (e) => {
+  const sendOrder = async () => {
 
-    if (name && phone && phone.length === 10 && parseInt(phone) && email && email.includes('@') && verified) {
-      e.preventDefault()
+    if (name && phone && phone.length === 10 && parseInt(phone) && email && email.includes('@') && cart.length > 0 && verified) {
+
       setLoading(true)
 
-      if (cart.length > 0) {
-        const order = {
-          buyer: { name: name, phone: phone, email: email },
-          items: cart.map(item => {
-            return { id: item.id, title: item.title, price: item.price, quantity: item.quantity }
-          }),
-          date: new Date(),
-          total: cart.reduce((accumulator, currentValue) => accumulator + (currentValue.quantity * currentValue.price), 0)
-        }
-
-        const db = getFirestore()
-
-        const ordersCollection = collection(db, 'orders')
-
-        try {
-          const { id } = await addDoc(ordersCollection, order)
-          setOrderId(id)
-
-          // Update item stock
-          const itemsCollection = collection(db, 'items')
-          cart.forEach(async (item) => {
-            const itemRef = doc(itemsCollection, item.id)
-            await updateDoc(itemRef, { stock: item.stock - item.quantity })
-          })
-
-          clear()
-        } catch (error) {
-          console.error('No se pudo crear la orden.')
-        }
-        setLoading(false)
+      const order = {
+        buyer: { name: name, phone: phone, email: email },
+        items: cart.map(item => {
+          return { id: item.id, title: item.title, price: item.price, quantity: item.quantity }
+        }),
+        date: new Date(),
+        total: cart.reduce((accumulator, currentValue) => accumulator + (currentValue.quantity * currentValue.price), 0)
       }
+
+      const db = getFirestore()
+
+      const ordersCollection = collection(db, 'orders')
+
+      try {
+        const { id } = await addDoc(ordersCollection, order)
+        setOrderId(id)
+
+        // Update item stock
+        const itemsCollection = collection(db, 'items')
+        cart.forEach(async (item) => {
+          const itemRef = doc(itemsCollection, item.id)
+          await updateDoc(itemRef, { stock: item.stock - item.quantity })
+        })
+
+        clear()
+      } catch (error) {
+        console.error('No se pudo crear la orden:', error)
+      }
+      setLoading(false)
+
     } else {
-      console.error('No se pudo crear la orden. Completar todos los campos requeridos.')
+      console.error('No se pudo crear la orden. Completar todos los campos requeridos y asegurarse de tener productos en el carrito.')
     }
   }
 
@@ -78,7 +77,14 @@ function Checkout() {
         handleName={handleName}
         handlePhone={handlePhone}
         handleEmail={handleEmail}
-        handleCaptcha={handleCaptcha} />
+        handleCaptcha={handleCaptcha}
+        orderId={orderId}
+        cart={cart}
+        name={name}
+        phone={phone}
+        email={email}
+        verified={verified}
+      />
     )
   } else {
     return (
