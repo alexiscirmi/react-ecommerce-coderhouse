923652e89921c2fc06b162946665a93eb9cb05ff
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-09 18:03:08 -0300

    Update project name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/coderhouse-react-project/',
+  base: '/react-coderhouse-project/',
   server: { host: true },
   plugins: [react()],
 })
