544a3f09635635ea5a7de115896a9acd7f74d66d
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-12-12 17:01:48 -0300

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/react-coderhouse-project/',
+  base: '/react-ecommerce-coderhouse/',
   server: { host: true },
   plugins: [react()],
 })
